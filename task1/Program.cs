2bd75b2ba5270ba963c5c9634aa818bcd4dc4b3b
Vika <vpredko@mail.ru>
2022-07-18 20:53:49 +0300

    Solve task1

---
+++ task1/Program.cs
@@ -0,0 +1,35 @@
+﻿double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble()*(10);
+        }
+    }
+    return result;
+    
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int row = 0; row < inArray.GetLength(0); row++)
+    {
+        for (int column = 0; column < inArray.GetLength(1); column++)
+        {
+            Console.Write($"{inArray[row,column]:f1}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите количество строк массива: ");
+int rows=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=Convert.ToInt32(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, 1, 10);
+PrintArray(array);
